e919af70a1fe30b76829ec1df4bcfd28161069f6
Sarah <sasooemad2014@gmail.com>
2025-05-03 21:10:12 +0300

    .

--- script.js
+++ script.js
@@ -112,19 +112,21 @@ window.addEventListener('DOMContentLoaded', () => {
     });
   }
 });
-const toggleBtn = document.getElementById('themeToggle');
-const isDark = localStorage.getItem('theme') === 'dark';
+document.addEventListener("DOMContentLoaded", () => {
+  const toggleBtn = document.getElementById('themeToggle');
+  const isDark = localStorage.getItem('theme') === 'dark';
 
-if (isDark) {
-document.body.classList.add('dark');
-toggleBtn.textContent = '☀️ Light Mode';
-}
+  if (isDark) {
+    document.body.classList.add('dark');
+    toggleBtn.textContent = '☀️ Light Mode';
+  }
 
-toggleBtn.addEventListener('click', () => {
-document.body.classList.toggle('dark');
-const darkMode = document.body.classList.contains('dark');
-toggleBtn.textContent = darkMode ? '☀️ Light Mode' : '🌙 Dark Mode';
-localStorage.setItem('theme', darkMode ? 'dark' : 'light');
+  toggleBtn.addEventListener('click', () => {
+    document.body.classList.toggle('dark');
+    const darkMode = document.body.classList.contains('dark');
+    toggleBtn.textContent = darkMode ? '☀️ Light Mode' : '🌙 Dark Mode';
+    localStorage.setItem('theme', darkMode ? 'dark' : 'light');
+  });
 });
 
 
